c783b558baa17d4bea98f60f1849bdeaf87c3691
A-M-I <160208713+Alisha000111@users.noreply.github.com>
2024-05-01 12:38:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+//let carsName = ["Dodge Challenger","Hellcat","La Ferrari"]
+//console.log(carsName);
+//carsName.push("Porsche 911")
+//console.log(carsName);
+//carsName.pop()
+//console.log(carsName);
+//carsName.unshift("Mehran")
+//console.log(carsName);
+//carsName.shift()
+//console.log(carsName);
+//let arr:string[] = ["apple","banana","cherry","kiwi","water melon"]
+//let newArr:string[]= arr.slice(1,3)
+//console.log(newArr)
+//let arr2:string[] = ["apple","banana","cherry","kiwi","water melon"]
+//let deletedElements = arr2.splice(1,2,"mango","peach")
+//console.log(deletedElements)
+var studentsName = ["Alisha", "Tania", "Asma", "Reshma", "Ifra", "Urossa", "Irfan", "Subhan", "Saaim", "Haroon"];
+//console.log(studentsName[9]);
+console.log(studentsName);
